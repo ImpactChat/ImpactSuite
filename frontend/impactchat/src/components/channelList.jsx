da2cbb31868e3a5443af03d60d74737db04fec69
tommcn <tommcn@mcnamer.ca>
2020-08-16 19:36:21 -0400

    Channel List as tabs

--- frontend/impactchat/src/components/channelList.jsx
+++ frontend/impactchat/src/components/channelList.jsx
@@ -5,6 +5,11 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 
 import Icon from '@material-ui/core/Icon';
@@ -16,6 +21,11 @@ const useStyles = makeStyles((theme) => ({
         maxHeight: '100%',
         width: '100%',
         backgroundColor: theme.palette.background.paper,
+        flexGrow: 1,
+        display: 'flex',
+        '& button': {
+            maxWidth: '100%'
+        }
     },
     classLogo: {
         margin: 'auto',
@@ -25,6 +35,10 @@ const useStyles = makeStyles((theme) => ({
     },
     nested: {
         paddingLeft: theme.spacing(4),
+    },
+    tabs: {
+        maxWidth: '100%',
+        borderRight: `1px solid ${theme.palette.divider}`,
     },
     
 }));
@@ -85,6 +99,11 @@ export default function ChannelList() {
     const [selectedIndex, setSelectedIndex] = React.useState(channelList.channels[0].channelID);
 
     // const [open, setOpen] = React.useState([]);
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+      };
+    
 
 
     const handleListItemClick = (event, index) => {
@@ -123,10 +142,69 @@ export default function ChannelList() {
             </List>
         );
     };
+    const generateTabList = function(list, idx) {
+        if (list.length === 0)
+            return
+        return (
+            <div className={classes.root}>
+                <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+                >
+                    <Tab label="Item One" {...a11yProps(0)} />
+                    <Tab label="Item Two" {...a11yProps(1)} />
+                    <Tab label="Item Three" {...a11yProps(2)} />
+                    <Tab label="Item Four" {...a11yProps(3)} />
+                    <Tab label="Item Four Four Four Four Four Four Four Four Four Four Four Four Four Four Four Four Four Four" {...a11yProps(4)} />
+                    <Tab label="Item Six" {...a11yProps(5)} />
+                    <Tab label="Item Seven" {...a11yProps(6)} />
+                </Tabs>
+        </div>
+        );
+    };
 
     return (
         <div className={classes.root}>
-            {generateList(channelList.channels, 0)}
+            {generateTabList(channelList.channels, 0)}
         </div>
     );
 }
+
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
+  
