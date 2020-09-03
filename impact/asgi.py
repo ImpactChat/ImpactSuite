873d154f27b58600dd56666b6c268bcaeb59db21
tommcn <tommcn@mcnamer.ca>
2020-09-03 10:55:24 -0400

    Fix settings

--- impact/asgi.py
+++ impact/asgi.py
@@ -18,6 +18,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "impact.settings")
 django.setup()
 application = get_default_application()
