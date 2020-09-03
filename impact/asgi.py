92853dfd222e31604302f54b4f31bb8223ed4335
tommcn <tommcn@mcnamer.ca>
2020-09-03 10:54:32 -0400

    Change default ASGI app

--- impact/asgi.py
+++ impact/asgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'impact.settings')
+
+# application = get_asgi_application()
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'impact.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+application = get_default_application()
