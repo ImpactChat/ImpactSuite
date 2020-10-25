f2b9790221e77393bff5a17311dcd56ba20af13d
tommcn <tommcn@mcnamer.ca>
2020-10-25 10:20:29 -0400

    p3

--- impact/settings.py
+++ impact/settings.py
@@ -119,12 +119,8 @@ if (os.environ.get("RUNNING_DOCKER", None) is not None):
         'PORT': '5432',
     }
 
-print(os.environ.get("DATABASE_URL", None))
 if (os.environ.get("DATABASE_URL", None) is not None):
     DATABASES['default'] = dj_database_url.parse(os.environ.get("DATABASE_URL", None), ssl_require=True, engine="django.db.backends.postgresql")
-print(DATABASES)
-print(os.environ.get("DATABASE_URL", None))
-print(dj_database_url.parse(os.environ.get("DATABASE_URL", None), ssl_require=True, engine="django.db.backends.postgresql"))
 
 
 
