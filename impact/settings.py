b933f8869d16be18a4b33929c29cae9a7d27d774
tommcn <tommcn@mcnamer.ca>
2020-10-25 09:32:19 -0400

    p3

--- impact/settings.py
+++ impact/settings.py
@@ -121,8 +121,7 @@ if (os.environ.get("RUNNING_DOCKER", None) is not None):
 
 print(os.environ.get("DATABASE_URL", None))
 if (os.environ.get("DATABASE_URL", None) is not None):
-    DATABASES['default'] = dj_database_url.config(ssl_require=True) 
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
+    DATABASES['default'] = dj_database_url.parse(os.environ.get("DATABASE_URL", None), ssl_require=True)
 print(DATABASES)
 
 if 'test' in sys.argv:
